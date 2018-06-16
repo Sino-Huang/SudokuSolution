dae5f0f6ab1d0ff2ac8bc2814215282764f6cce4
Sukai <u6492211@anu.edu.au>
2018-06-17 00:10:39 +1000

    add some test

--- src/sample/Sudoku.java
+++ src/sample/Sudoku.java
@@ -1,5 +1,9 @@
 package sample;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Sudoku {
     public static final int[][] box = {
             {1, 1, 1, 2, 2, 2, 3, 3, 3},
@@ -21,19 +25,40 @@ public class Sudoku {
         return null;
     }
 
-    public int[] getRow(int row) {
+    private int[] getRow(int row) {
         return null;
     }
 
-    public int[] getCol(int col) {
+    private int[] getCol(int col) {
         return null;
     }
 
-    public int[] getBox(int row, int col) {
+    private int[] getBox(int row, int col) {
         return null;
     }
 
-    public int[] conjunction(int[] rows, int[] cols, int[] boxes) {
-        return null;
+    public Integer[] conjunction(int[] rows, int[] cols, int[] boxes) {
+        Set<Integer> rowSet = getSet(rows);
+        Set<Integer> temp = getSet(cols);
+        rowSet.retainAll(temp);
+        temp = getSet(boxes);
+        rowSet.retainAll(temp);
+        Integer[] output = rowSet.toArray(new Integer[0]);
+
+        return output;
     }
+
+    private Set<Integer> getSet(int[] args) {
+        Set<Integer> output = new HashSet<>();
+        for (int i = 0; i < args.length; i++) {
+            output.add(args[i]);
+        }
+        return output;
+    }
+
+    public static void main(String[] args) {
+        Sudoku sudoku = new Sudoku(null);
+        System.out.println(Arrays.toString(sudoku.conjunction(new int[]{1, 2, 3, 4}, new int[]{1, 2, 3}, new int[]{3})));
+    }
+
 }
