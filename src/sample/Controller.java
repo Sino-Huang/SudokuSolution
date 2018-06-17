f803ea2bad1a5c9fed56d272b388caa0930928ad
Sukai <u6492211@anu.edu.au>
2018-06-17 11:43:04 +1000

    start javafx

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,18 @@
 package sample;
 
 public class Controller {
+    int[][] output;
+    public int[][] getOutput() {
+        int[][] map = new int[9][9];
+        for (int row = 0; row < 9; row++) {
+            map[row] = null;
+        }
+        Sudoku sudoku = new Sudoku(map);
+        output = sudoku.getSolution(sudoku.targetMap);
+        return output;
+    }
+
+    public void toOutput() {
+
+    }
 }
